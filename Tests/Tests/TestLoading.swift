1484e6c59f9f2abfbc903b03e3f0bc6b1ecd81e0
vauxhall <zero.griffin@gmail.com>
2017-09-21 14:08:24 -0500

    Test load url

--- Tests/Tests/TestLoading.swift
+++ Tests/Tests/TestLoading.swift
@@ -42,4 +42,48 @@ final class TestLoading:XCTestCase
                 "found error while loading")
         }
     }
+    
+    func testLoadUrl()
+    {
+        let loadExpectation:XCTestExpectation = expectation(
+            description:"load xml")
+        
+        let bundle:Bundle = Bundle(for:TestLoading.self)
+        
+        guard
+            
+            let url:URL = bundle.url(
+                forResource:kResourceName,
+                withExtension:kResourceExtension)
+        
+        else
+        {
+            return
+        }
+        
+        var loadedXml:Any?
+        var loadingError:XmlError?
+        
+        Xml.object(
+            url:url)
+        { (xml:Any?, error:XmlError?) in
+            
+            loadedXml = xml
+            loadingError = error
+            
+            loadExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout:kWaitExpectation)
+        { (error:Error?) in
+            
+            XCTAssertNotNil(
+                loadedXml,
+                "failed loading xml")
+            
+            XCTAssertNil(
+                loadingError,
+                "found error while loading")
+        }
+    }
 }

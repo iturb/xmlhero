1c28e7b778a8534ecd192da737494794bdb23685
vauxhall <zero.griffin@gmail.com>
2017-09-21 14:11:34 -0500

    Setup loading xml

--- Tests/Tests/TestElements.swift
+++ Tests/Tests/TestElements.swift
@@ -1,12 +1,25 @@
 import XCTest
+@testable import XmlHero
 
 final class TestElements:XCTestCase
 {
+    private var xml:Any?
     private let kResourceName:String = "mockElements"
     private let kResourceExtension:String = "xml"
     
     override func setUp()
     {
         super.setUp()
+        
+        let bundle:Bundle = Bundle(for:TestElements.self)
+        
+        Xml.object(
+            fileName:kResourceName,
+            withExtension:kResourceExtension,
+            bundle:bundle)
+        { (xml:Any?, error:XmlError?) in
+            
+            self.xml = xml
+        }
     }
 }

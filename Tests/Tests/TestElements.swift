4785033850b9d61307410a0e5bbd24c2716854f9
vauxhall <zero.griffin@gmail.com>
2017-09-21 14:05:08 -0500

    Test loading by file name

--- Tests/Tests/TestElements.swift
+++ Tests/Tests/TestElements.swift
@@ -2,6 +2,9 @@ import XCTest
 
 final class TestElements:XCTestCase
 {
+    private let kResourceName:String = "mockElements"
+    private let kResourceExtension:String = "xml"
+    
     override func setUp()
     {
         super.setUp()
--- Tests/Tests/TestLoading.swift
+++ Tests/Tests/TestLoading.swift
@@ -1,9 +1,45 @@
-//
-//  TestLoading.swift
-//  Tests
-//
-//  Created by zero on 9/21/17.
-//  Copyright © 2017 iturbide. All rights reserved.
-//
+import XCTest
+@testable import XmlHero
 
-import Foundation
+final class TestLoading:XCTestCase
+{
+    private let kResourceName:String = "mockElements"
+    private let kResourceExtension:String = "xml"
+    private let kWaitExpectation:TimeInterval = 2
+    
+    //MARK: tests
+    
+    private func testLoadFile()
+    {
+        let loadExpectation:XCTestExpectation = expectation(
+            description:"load xml")
+        
+        let bundle:Bundle = Bundle(for:TestLoading.self)
+        var loadedXml:Any?
+        var loadingError:XmlError?
+        
+        Xml.object(
+            fileName:kResourceName,
+            withExtension:kResourceExtension,
+            bundle:bundle)
+        { (xml:Any?, error:XmlError?) in
+            
+            loadedXml = xml
+            loadingError = error
+            
+            loadExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout:kWaitExpectation)
+        { (error:Error?) in
+            
+            XCTAssertNotNil(
+                loadedXml,
+                "failed loading xml")
+            
+            XCTAssertNil(
+                loadingError,
+                "found error while loading")
+        }
+    }
+}

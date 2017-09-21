a057d2e01a009e048f6754f142a0f2f837b36571
vauxhall <zero.griffin@gmail.com>
2017-09-21 02:19:15 -0500

    Add factory methods

--- Source/XmlFactory.swift
+++ Source/XmlFactory.swift
@@ -2,5 +2,28 @@ import Foundation
 
 extension Xml
 {
+    //MARK: open
     
+    open class func object(
+        fileName:String,
+        extension:String,
+        bundle:Bundle?,
+        completion:@escaping((Xml?, Error?) -> ()))
+    {
+        
+    }
+    
+    open class func object(
+        url:URL,
+        completion:@escaping((Xml?, Error?) -> ()))
+    {
+        
+    }
+    
+    open class func object(
+        data:Data,
+        completion:@escaping((Xml?, Error?) -> ()))
+    {
+        
+    }
 }

5cb0a3247c1d6432bb1290efc8c6f221e9791499
vauxhall <zero.griffin@gmail.com>
2017-09-21 02:46:18 -0500

    Create error factory

---
+++ Source/XmlErrorFactory.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+extension XmlError
+{
+    //MARK: internal
+    
+    static func fileNotFound() -> XmlError
+    {
+        let error:XmlError = XmlError()
+        
+        return error
+    }
+}

1004dceec2fe4f2fa677e767b9a407f595556895
vauxhall <zero.griffin@gmail.com>
2017-09-21 01:14:14 -0500

    Add init to view

--- Demo/View/View.swift
+++ Demo/View/View.swift
@@ -2,5 +2,15 @@ import UIKit
 
 class View:UIView
 {
+    init()
+    {
+        super.init(frame:CGRect.zero)
+        clipsToBounds = true
+        backgroundColor = UIColor.white
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }

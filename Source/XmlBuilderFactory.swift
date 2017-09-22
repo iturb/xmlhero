6d759e7c8a06ae3fe0e18a3203833b4eb80a7dc0
vauxhall <zero.griffin@gmail.com>
2017-09-22 12:44:38 -0500

    Factory xml declaration

--- Source/XmlBuilderFactory.swift
+++ Source/XmlBuilderFactory.swift
@@ -2,5 +2,12 @@ import Foundation
 
 extension XmlBuilder
 {
+    private static let kDeclaration:String = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"
+    
     //MARK: internal
+    
+    class func factoryHeader() -> String
+    {
+        return kDeclaration
+    }
 }

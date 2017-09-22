02070c7fe63ebf9625e6a1ba6246d8726049fc51
vauxhall <zero.griffin@gmail.com>
2017-09-22 11:42:03 -0500

    Add factory methods

--- Source/XmlFactoryData.swift
+++ Source/XmlFactoryData.swift
@@ -2,5 +2,33 @@ import Foundation
 
 extension Xml
 {
+    //MARK: private
     
+    private class func asyncData(
+        xml:Xml,
+        object:Any,
+        completion:@escaping((Data?, XmlError?) -> ()))
+    {
+        
+    }
+    
+    //MARK: internal
+    
+    @discardableResult open class func data(
+        object:Any,
+        completion:@escaping((Data?, XmlError?) -> ())) -> Xml
+    {
+        let xml:Xml = Xml()
+        
+        DispatchQueue.global(
+            qos:DispatchQoS.QoSClass.background).async
+        {
+            asyncData(
+                xml:xml,
+                object:object,
+                completion:completion)
+        }
+        
+        return xml
+    }
 }

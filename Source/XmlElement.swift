917bb0948f44b01111f2d5267862fed0386a8543
vauxhall <zero.griffin@gmail.com>
2017-09-22 10:05:29 -0500

    Add attributes to xml element

--- Source/XmlElement.swift
+++ Source/XmlElement.swift
@@ -4,6 +4,7 @@ final class XmlElement
 {
     let name:String
     private(set) weak var parent:XmlElement?
+    private(set) var attributes:[String:String]?
     private(set) var children:[XmlElement]?
     private(set) var value:String?
     
@@ -25,6 +26,16 @@ final class XmlElement
         }
         
         value?.append(string)
+    }
+    
+    func addAttribute(name:String, value:String)
+    {
+        if attributes == nil
+        {
+            attributes = [:]
+        }
+        
+        attributes?[name] = value
     }
     
     func addChild(child:XmlElement)
